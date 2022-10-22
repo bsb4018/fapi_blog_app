afd5294240fb1dbf177f7337b41e24f7ffa0e7dd
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-10-22 20:16:28 +0530

    create blog added

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from . database import engine
+from . database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -10,6 +11,18 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post('/blog')
-def create(request:schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
