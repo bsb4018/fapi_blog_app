d96d72321de9cbbedfafe1767af730e7e1b5b732
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-10-24 12:53:53 +0530

    Main Blog Created

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,15 +1,12 @@
 from pydantic import BaseModel
+from typing import List
 
 class Blog(BaseModel):
     title: str
     body: str
-
-class ShowBlog(BaseModel):
-    title: str
-    body: str
     class Config():
         orm_mode = True
-    
+
 
 class User(BaseModel):
     name: str
@@ -20,6 +17,14 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     name: str
     email: str
-
+    blogs: List[Blog]
     class Config():
         orm_mode = True     
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+    class Config():
+        orm_mode = True
